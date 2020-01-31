115492489667584d97464762a1ba8dfe437d919b
Kenny Nguyen <kenny.mboutrecht@gmail.com>
2020-01-31 22:37:19 +0100

    first commit

---
+++ Assets/Course Library/Scripts/GameManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject[] foodPrefabs;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    private float spawnRate = 1.0f;
+    private int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        scoreText.text = "Seconds: " + score;
+        UpdateScore(0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, foodPrefabs.Length);
+            Instantiate(foodPrefabs[index]);
+            UpdateScore(1);
+            
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Seconds: " + score;
+    }
+    
+}
---
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private GameManager gameManager;
+    public float horizontalInput;
+    public float speed = 15.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();    
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+        // Keep the player in bound
+
+        if (transform.position.x < -4)
+        {
+            transform.position = new Vector3(-4, transform.position.y, transform.position.z);
+        }
+        
+        if (transform.position.x > 4)
+        {
+            transform.position = new Vector3(4, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+    }
+    
+}
